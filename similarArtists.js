4678cfa96e40cbdffe9053c37cb170811e49da23
Imparato, Remo <Remo.Imparato+bltech@bottomline.com>
2026-01-17 13:02:43 -0600

    js syntax fix

--- similarArtists.js
+++ similarArtists.js
@@ -807,7 +807,9 @@
 		if (!arr || arr.length <= 1) return;
 		for (let i = arr.length - 1; i > 0; --i) {
 			const j = Math.floor(Math.random() * (i + 1));
-			[arr[i], arr[j]] = [arr[j], arr[i]);
+			const temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
 		}
 	}
 
